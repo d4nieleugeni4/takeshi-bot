f0d3a4c1960f0be76bda6b50cd6f3b1e2996389d
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-02-09 10:09:50 -0300

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -6,27 +6,27 @@
 const { version } = require("../../package.json");
 
 exports.sayLog = (message) => {
-  console.log("\x1b[36m[TAKESHI BOT | TALK]\x1b[0m", message);
+  console.log("\x1b[36m[JARVIS | TALK]\x1b[0m", message);
 };
 
 exports.inputLog = (message) => {
-  console.log("\x1b[30m[TAKESHI BOT | INPUT]\x1b[0m", message);
+  console.log("\x1b[30m[JARVIS | INPUT]\x1b[0m", message);
 };
 
 exports.infoLog = (message) => {
-  console.log("\x1b[34m[TAKESHI BOT | INFO]\x1b[0m", message);
+  console.log("\x1b[34m[JARVIS | INFO]\x1b[0m", message);
 };
 
 exports.successLog = (message) => {
-  console.log("\x1b[32m[TAKESHI BOT | SUCCESS]\x1b[0m", message);
+  console.log("\x1b[32m[JARVIS | SUCCESS]\x1b[0m", message);
 };
 
 exports.errorLog = (message) => {
-  console.log("\x1b[31m[TAKESHI BOT | ERROR]\x1b[0m", message);
+  console.log("\x1b[31m[JARVIS | ERROR]\x1b[0m", message);
 };
 
 exports.warningLog = (message) => {
-  console.log("\x1b[33m[TAKESHI BOT | WARNING]\x1b[0m", message);
+  console.log("\x1b[33m[JARVIS | WARNING]\x1b[0m", message);
 };
 
 exports.bannerLog = () => {
