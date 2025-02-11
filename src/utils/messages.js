100700e092b08dd7e345fce3999b128c796419f0
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-02-11 08:15:35 -0300

    Update messages.js

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -10,44 +10,45 @@ exports.waitMessage = "Carregando dados...";
 exports.menuMessage = () => {
   const date = new Date();
 
-  return `╭━━⫸ ✮BEM VINDO!✮ ⫷━━
-▢
-▢ • ${BOT_NAME}
-▢ • Data: ${date.toLocaleDateString("pt-br")}
-▢ • Hora: ${date.toLocaleTimeString("pt-br")}
-▢ • Prefixo: ${PREFIX}
-▢
-╰━━─「🪐」─━━
+  return `┏━━⫸ ✮BEM VINDO!✮ ⫷━━┓
+┃                       ┃
+┃➤ ${BOT_NAME}              ┃
+┃➤ Data: ${date.toLocaleDateString("pt-br")}              ┃
+┃➤ Hora: ${date.toLocaleTimeString("pt-br")}              ┃
+┃➤ Prefixo: ${PREFIX}           ┃
+┃                       ┃
+┗━━━━━━━━「🪐」━━━━━━━━┛
 
-╭━━⫸ ★DONO (DN)★ ⫷━━
-▢
-▢ • ${PREFIX}off
-▢ • ${PREFIX}on
-▢
-╰━━─「🌌」─━━
+┏━━⫸ ★DONO (DN)★ ⫷━━┓
+┃                      ┃
+┃➤ ${PREFIX}off               ┃
+┃➤ ${PREFIX}on                ┃
+┃                      ┃
+┗━━━━━━━━「🌌」━━━━━━━━┛
 
-╭━━⫸ ✮ADMINS✮ ⫷━━
-▢
-▢ • ${PREFIX}anti-link (1/0)
-▢ • ${PREFIX}auto-responder (1/0)
-▢ • ${PREFIX}ban
-▢ • ${PREFIX}hidetag
-▢ • ${PREFIX}welcome (1/0)
-▢
-╰━━─「⭐」─━━
+┏━━━━⫸ ✮ADMINS✮ ⫷━━━━┓
+┃                      ┃
+┃➤ ${PREFIX}anti-link (1/0)    ┃
+┃➤ ${PREFIX}auto-responder (1/0)┃
+┃➤ ${PREFIX}ban                ┃
+┃➤ ${PREFIX}hidetag            ┃
+┃➤ ${PREFIX}welcome (1/0)      ┃
+┃                      ┃ 
+┗━━━━━━━━「⭐」━━━━━━━━┛
 
-╭━━⫸ ✮MENU✮ ⫷━━
-▢
-▢ • ${PREFIX}attp
-▢ • ${PREFIX}cep
-▢ • ${PREFIX}gpt-4
-▢ • ${PREFIX}ia-sticker
-▢ • ${PREFIX}image
-▢ • ${PREFIX}ping
-▢ • ${PREFIX}play-audio
-▢ • ${PREFIX}play-video
-▢ • ${PREFIX}sticker
-▢ • ${PREFIX}to-image
-▢
-╰━━─「🚀」─━━`;
+
+┏━━⫸ ✮MENU✮ ⫷━━┓
+┃                ┃         
+┃➤ ${PREFIX}attp        ┃
+┃➤ ${PREFIX}cep         ┃
+┃➤ ${PREFIX}gpt-4       ┃
+┃➤ ${PREFIX}ia-sticker  ┃
+┃➤ ${PREFIX}image       ┃
+┃➤ ${PREFIX}ping        ┃
+┃➤ ${PREFIX}play-audio  ┃
+┃➤ ${PREFIX}play-video  ┃
+┃➤ ${PREFIX}sticker     ┃
+┃➤ ${PREFIX}to-image    ┃
+┃                ┃ 
+┗━━━━━「🚀」━━━━━┛`;
 };
